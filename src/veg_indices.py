1e06ab06ffbd7c96fc7192bb8dff84af1ea2639f
Tara Cunningham <tcc78128@gmail.com>
2021-07-16 15:54:06 +0100

    aoi fiona code added to ndvi fn, but not changed to my scenario yet.

--- src/veg_indices.py
+++ src/veg_indices.py
@@ -7,15 +7,18 @@ from tree_mask import all
 import numpy as np
 import matplotlib.pyplot as plt
 import rasterio as rio
+from rasterio.tools.mask import mask
+import fiona
 
 
-def ndvi(filepath, aoi):
+def ndvi(filepath, aoi = None):
     '''
     A function to calculate ndvi = (B8 - B4) / (B8 + B4)
 
     Parameters
     ----------
-    filepath to 
+    filepath: to folder containing individual bands
+    aoi: optional parameter to crop raster to an area of interest by providing full path to a shapefile
 
     Returns
     ----------
@@ -25,6 +28,25 @@ def ndvi(filepath, aoi):
     # for testing
     filepath = '/Users/taracunningham/projects/dissertation/sen2processing/processing/l2a/S2A_MSIL2A_20180514T073611_N9999_R092_T36MZC_20210520T183726.SAFE/GRANULE/L2A_T36MZC_A015104_20180514T075528/IMG_DATA/R10m'
 
+    if aoi is not None:
+        with fiona.open("/Users/Cate/UK_Mainland.shp", "r") as shapefile:
+            geoms = [feature["geometry"] for feature in shapefile]
+            # can do it with the crop=True thing I think
+
+        with rasterio.open("jan_clip.tif") as src:
+            out_image, out_transform = mask(src, geoms, crop=True)
+            out_meta = src.meta.copy()
+
+        out_meta.update({"driver": "GTiff",
+                 "height": out_image.shape[1],
+                 "width": out_image.shape[2],
+                 "transform": out_transform})
+
+        with rasterio.open("masked2.tif", "w", **out_meta) as dest:
+            dest.write(out_image)
+    
+
+
     # read in red and NIR bands
     red = rio.open(f'{filepath}/T36MZC_20180514T073611_B04_10m.jp2', driver ='JP2OpenJPEG')
     nir = rio.open(f'{filepath}/T36MZC_20180514T073611_B08_10m.jp2', driver ='JP2OpenJPEG')
@@ -33,8 +55,8 @@ def ndvi(filepath, aoi):
     kwargs = red.meta
 
     # read values to do band maths
-    b04 = red.read(1, masked = True).astype('float64')
-    b08 = nir.read(1, masked = True)
+    b04 = red.read(1, masked = True).astype('float64') # change dtype to float in prep for ndvi calc
+    b08 = nir.read(1, masked = True).astype('float64')
 
     # close files
     red.close()
@@ -49,16 +71,27 @@ def ndvi(filepath, aoi):
         driver = 'Gtiff'
         )
 
-# %%
     # write out ndvi test
-    with rio.open('/Users/taracunningham/projects/dissertation/sen2processing/processing/l2a/ndvi_test.tif', 'w', **kwargs) as dst:
+    with rio.open('/Users/taracunningham/projects/dissertation/sen2processing/processing/l2a/ndvi_test2.tif', 'w', **kwargs) as dst:
         dst.write_band(1, ndvi)
 
-    # values not below zero anywhere. check stats. Divide by 10000? Shouldn't matter because of the ratio. Only matters in because formula adds numbers.
 
     '''
     rasters = [rasterio.open(p) for p in file_paths]
     stacked = np.dstack([r.read() for r in rasters])
+    '''
+
+#######################################
+
+def check_crs():
+    '''
+    check two crs are the same. Return boolean
+    '''
+
+#######################################
+
+def reproject_crs(file, dst_crs):
+    '''
     '''
     
 
