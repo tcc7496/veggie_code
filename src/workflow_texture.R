b1ea2c1512e847dc9c29f8e8de8a6db3968e5cd8
Tara Cunningham <tcc78128@gmail.com>
2021-08-13 11:44:06 +0100

    rescaling evi data to 8bit 0-255 interval function added and calculating glcm with n256

--- src/workflow_texture.R
+++ src/workflow_texture.R
@@ -66,6 +66,12 @@ rescale.to.integers <- function(r) {
   return(r)
 }
 
+# rescale veg index data to 0-255 range and convert to integers
+# assumes range of veg. index is [0,1]
+rescale.to.eightbit <- function(r, rmin = 0, rmax = 1) {
+  r <- as.integer(round(f * 255.0))
+}
+
 # calculate texture metrics function with defaults
 calc.textures <- function(r, stats = c('homogeneity', 'variance'), n_grey = 101) {
   r_textures <- glcm(r[[1]], window = c(3,3), n_grey = n_grey,
@@ -93,10 +99,10 @@ treemask_file <- 'tree_mask/tree_mask_species_map_4_inverse_buffered.tif'
 swamp_file <- 'swamp_shapefiles/swamp_clipped.shp'
 
 # define output file paths
-outpaths_hmg <- list('texture_metrics/from_evi/hmg_2017_rescaled_n100_any.tif',
-                     'texture_metrics/from_evi/hmg_2018_rescaled_n100_any.tif')
-outpaths_var <- list('texture_metrics/from_evi/var_2017_rescaled_n100_any.tif',
-                     'texture_metrics/from_evi/var_2018_rescaled_n100_any.tif')
+outpaths_hmg <- list('texture_metrics/from_evi/hmg_2017_rescaled_to_eightbit_n256_any.tif',
+                     'texture_metrics/from_evi/hmg_2018_rescaled_to_eightbit_n256_any.tif')
+outpaths_var <- list('texture_metrics/from_evi/var_2017_rescaled_to_eightbit_n256_any.tif',
+                     'texture_metrics/from_evi/var_2018_rescaled_to_eightbit_n256_any.tif')
 
 # open files the same for all years
 treemask <- open.band(treemask_file) # trees where treemask = 1
@@ -115,11 +121,11 @@ for (i in 1:length(image_files_evi)) {
     mask.raster.with.shp(swamp_shp) %>%
     mask.raster.with.raster(treemask) %>%
     rescale.outliers() %>%
-    rescale.to.integers
+    rescale.to.eightbit
   
   # calculate texture metrics
   tic('glcm calculation, 3x3 window')
-  evi_glcm <- calc.textures(evi_image_prepped)
+  evi_glcm <- calc.textures(evi_image_prepped, n_grey = 256)
   toc()
 
   # write out rasters
@@ -142,10 +148,11 @@ diff_wet_dry_var <- diff_rasters(evi_2018$glcm_variance, evi_2017$glcm_variance)
 #### Write out
 
 # define output file names
-outpath_hmg_diff <- 'texture_metrics/from_evi/diff_wet2018_minus_dry2017_hmg_rescaled_n100_any.tif'
-outpath_var_diff <- 'texture_metrics/from_evi/diff_wet2018_minus_dry2017_var_rescaled_n100_any.tif'
+outpath_hmg_diff <- 'texture_metrics/from_evi/diff_wet2018_minus_dry2017_hmg_rescaled_to_eightbit_n256_any.tif'
+outpath_var_diff <- 'texture_metrics/from_evi/diff_wet2018_minus_dry2017_var_rescaled_to_eightbit_n256_any.tif'
 
 # write out rasters
+# NB overwrites files if they already exist
 writeRaster(diff_wet_dry_hmg, filename = paste0(wd, outpath_hmg_diff),
             format = 'GTiff', overwrite = TRUE)
 writeRaster(diff_wet_dry_var, filename = paste0(wd, outpath_var_diff),
