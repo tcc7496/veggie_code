5fb3fb0a7f6c32d3c56774183dea731601c9b972
Tara Cunningham <tcc78128@gmail.com>
2021-09-17 17:47:42 +0100

    file in wrong place

--- src/texture_calc.R
+++ src/texture_calc.R
@@ -1,5 +1,5 @@
 #
-# Script to calculate texture metrics
+# A script to calculate texture metrics
 #
 
 #### Libraries ----
@@ -9,25 +9,14 @@ options("rgdal_show_exportToProj4_warnings"="none")
 library(rgdal)
 library(sp)
 library(tictoc)     # for timing operations
-library(reticulate) # for python integration
-library(viridis)    # for plotting
+library(viridis)
 library(tidyr)
-library(ggplot2)
-library(mapproj)
-library(proj4)
-library(ggalt)
 library(sf)
 library(exactextractr)
 library(glue)
 library(purrr)
 library(stringr)
-library(lme4)
-library(nlme)
-library(glmm)
 library(tibble)
-library(patchwork)
-library(MASS)
-
 
 ## Working Dir ----
 
@@ -91,7 +80,7 @@ calc.zonal.stats.batch(inputdir = hmg_fp,
 
 # Do 2 runs, one with the only the same pixels across all the years, one with all available pixels
 # If normalise = T, output csv contains absolute and normalised values values
-# Also outputs a csv file landuse_normalised_values.csv which contains the mean value of homogeneity across all years
+# Also outputs a csv file landuse_mean_values.csv which contains the mean value of homogeneity across all years
 inputdir = 'texture_metrics/from_evi/hmg/abs/'
 outputdir_abs = inputdir
 outputdir_norm = 'texture_metrics/from_evi/hmg/norm/'
@@ -126,12 +115,65 @@ values_per_landuse_norm <- extract.values.by.polygon.batch(
 # define filepaths
 inputdir = 'texture_metrics/from_evi/hmg/abs/'
 outputdir = 'texture_metrics/from_evi/hmg/norm/'
-statsfile <- 'zs_from_evi_abs.gpkg'
-meanfile <- '../landuse_normalised_values.csv'
+statsfile <- 'zs_from_evi_abs.gpkg'               # homogeneity zonal stats per land use for all years
+meanfile <- '../landuse_mean_values.csv'    # mean values per landuse across all years
 
 # function call
 calc.normalised.hmg.raster(inputdir, outputdir, statsfile, meanfile)
 
 
+## Perform Mann-Whitney U test ----
+# equivalent to Wilcoxon Rank Sum test
 
+# import homogeneity data
+hmg_fp <- 'texture_metrics/from_evi/hmg/norm/'
+inputfile <- paste0(hmg_fp, 'hmg_norm_same_pixels_F.csv')
 
+hmg_df <- read.csv2(inputfile)
+
+# Wilcoxon test
+# formula: homogeneity ~ land use
+
+# for all years
+wil_test_all_yrs <- wilcox.test(
+  hmg_df$value ~ hmg_df$LandUse,
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
+
+# within each year
+wil_test_2016 <- wilcox.test(
+  hmg_df$value[hmg_df$year == 2016] ~ hmg_df$LandUse[hmg_df$year == 2016],
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
+
+wil_test_2017 <- wilcox.test(
+  hmg_df$value[hmg_df$year == 2017] ~ hmg_df$LandUse[hmg_df$year == 2017],
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
+
+wil_test_2018 <- wilcox.test(
+  hmg_df$value[hmg_df$year == 2018] ~ hmg_df$LandUse[hmg_df$year == 2018],
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
+
+wil_test_2019 <- wilcox.test(
+  hmg_df$value[hmg_df$year == 2019] ~ hmg_df$LandUse[hmg_df$year == 2019],
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
+
+wil_test_2020 <- wilcox.test(
+  hmg_df$value[hmg_df$year == 2020] ~ hmg_df$LandUse[hmg_df$year == 2020],
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
+
+wil_test_2021 <- wilcox.test(
+  hmg_df$value[hmg_df$year == 2021] ~ hmg_df$LandUse[hmg_df$year == 2021],
+  alternative = 'two.sided',
+  conf.int = T,
+  digits.rank = 4)
